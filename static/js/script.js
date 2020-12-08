ed4cc78d0680cd9281ff4a3a09e7d930bae10506
Adrian Looney <adrian.looney@gmail.com>
2020-12-08 15:49:24 +0000

    Updated individual movie page to display no poster image

--- static/js/script.js
+++ static/js/script.js
@@ -3,14 +3,5 @@ $(document).ready(function(){
   $('.modal').modal();
   $('select').formSelect();
   $('.tooltipped').tooltip();
-  $('.datepicker').datepicker({
-      format: "yyyy",
-      yearRange: 120,
-      maxDate: new Date(2025,7,14),
-      showClearBtn: true,
-      i18n: {
-          done: "Select"
-      }
-  })
-})
-
+  $('.carousel').carousel();
+});
