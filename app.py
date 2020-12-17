339e28fd1eacf42418b9fd42d17b663ec145426c
Adrian Looney <adrian.looney@gmail.com>
2020-12-17 21:14:21 +0000

    Final minor changes

--- app.py
+++ app.py
@@ -166,4 +166,4 @@ def logout():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT', 8000)),
-            debug=True)
+            debug=False)
